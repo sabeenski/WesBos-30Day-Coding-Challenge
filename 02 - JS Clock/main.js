2bd45a76e59df6db3969205032982a8cd2987a57
sabeenski <sabinadhaugoda@gmail.com>
2019-02-09 16:26:39 +0100

    Clock Challenge

---
+++ 02 - JS Clock/main.js
@@ -0,0 +1,33 @@
+
+
+const secondHand = document.querySelector('.second-hand')
+const minuteHand = document.querySelector('.minute-hand')
+const hourHand = document.querySelector('.hour-hand')
+const handsAll = document.querySelectorAll('.hand')
+
+function setDate() {
+  const now = new Date()
+
+  const seconds = now.getSeconds()
+  const secondsDegrees = ((seconds / 60) * 360) + 90
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+  console.log(seconds)
+
+  const minutes = now.getMinutes()
+  const minutesDegrees = ((minutes / 60) * 360) + 90
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+  console.log(minutes)
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  if(secondsDegrees === 90) {
+    allHands.forEach(hand => hand.style.transition === 'none')
+  } else {
+    allHands.forEach(hand => hand.style.transition = '')
+  }
+
+}
+
+setInterval(setDate, 1000) 
